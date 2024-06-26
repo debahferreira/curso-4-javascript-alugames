0c5815e3252993c75dc61b2dbf3a7a0894e1009b
Deborah Ferreira <debahferreira2024@gmail.com>
2024-06-26 15:06:29 -0300

    desafios 1 e 2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
+let listaDeAlugados = ['alugado'];
+
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let imagemJogo = jogo.querySelector('.dashboard__item__img');
     let botaoJogo = jogo.querySelector('.dashboard__item__button');
+    let nomeJogo = jogo.querySelector('.dashboard__item__name');
     
     if(imagemJogo.classList.contains('dashboard__item__img--rented')) {
+        alert(`Você confirma a devolução do jogo ${nomeJogo.textContent}?`);
         imagemJogo.classList.remove('dashboard__item__img--rented');
         botaoJogo.classList.remove('dashboard__item__button--return');
         botaoJogo.innerHTML = 'Alugar';
+        listaDeAlugados.pop();
+        quantidadeDeAlugados();
     } else {
         imagemJogo.classList.add('dashboard__item__img--rented');
         botaoJogo.classList.add('dashboard__item__button--return');
         botaoJogo.innerHTML = 'Devolver';
+        listaDeAlugados.push('alugado');
+        quantidadeDeAlugados();
     }
+}
+
+function quantidadeDeAlugados() {
+    console.log(`A quantidade de jogos alugados é ${listaDeAlugados.length}.`);
 }
